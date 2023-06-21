096cf98d0aa49c4d2042a6905de728a5a180548d
elder sarmiento <eldersarmiento1@gmail.com>
2023-06-20 20:47:02 -0500

    Data validation for entered login information has been added.

--- app/src/main/java/com/example/indentproject2/Login.java
+++ app/src/main/java/com/example/indentproject2/Login.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -20,20 +21,58 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
 
-        usuario = findViewById(R.id.usuario);
-        contrasena = findViewById(R.id.contrasena);
+        EditText usuario = findViewById(R.id.usuario);
+        usuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                usuario.setHint("");
+            }
+        });
+
+        EditText contrasena = findViewById(R.id.contrasena);
+        contrasena.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                contrasena.setHint("");
+            }
+        });
+
         btnLogin = findViewById(R.id.btnLogin);
+
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String nombreUsuario = usuario.getText().toString();
+                String usuario1 = usuario.getText().toString();
+                String contrasena1 = contrasena.getText().toString();
+                String usuarioDB = "pepito@mail.com";
+                String contrasenaDB = "xyz123";
 
-                Intent intent = new Intent(Login.this, mainPage.class);
-                intent.putExtra("keyword", nombreUsuario);
-                startActivity(intent);
+                iniciarSesion(usuario1,contrasena1, usuarioDB,contrasenaDB);
+
             }
         });
+    }
+
+    public void iniciarSesion(String usuario, String contrasena, String usuarioDB, String contrasenaDB){
+
+        if (usuario.equals(usuarioDB) && contrasena.equals(contrasenaDB)){
+
+            Intent intent = new Intent(Login.this, mainPage.class);
+
+            intent.putExtra("keyword", usuario);
+
+            startActivity(intent);
+
+            Toast.makeText(Login.this, "Has Iniciado sesión", Toast.LENGTH_SHORT).show();
+
+        }else{
+
+            System.out.println("Valide sus credenciales");
+
+            Toast.makeText(this, "Validar credenciales", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
--- app/src/main/java/com/example/indentproject2/Registro.java
+++ app/src/main/java/com/example/indentproject2/Registro.java
@@ -3,12 +3,20 @@ package com.example.indentproject2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Registro extends AppCompatActivity {
+    Button button;
+    EditText nombre;
+    EditText apellido;
+    EditText correo;
+    EditText contrasena;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
     }
 }
